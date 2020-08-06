bc35265027b29d4e0efa284a4eb95caa5d2d2bbd
Nathan Donolli <ndonolli@gmail.com>
2020-08-06 02:21:50 -0500

    add bulma and styles

--- src/main/winkler_demo/views.cljs
+++ src/main/winkler_demo/views.cljs
@@ -12,39 +12,75 @@
 (defn roll-display [rolls]
   [:p "your rolls are: " (apply str (interpose ", " rolls))])
 
+
+(defn header []
+  [:section.hero.is-dark
+   [:div.hero-body
+    [:div.container
+     [:h1.title "Winkler's DnD Dice"]
+     [:h2.subtitle "Making random rolls somehow more random"]]]])
+
 (defn roller []
   (let [roll (r/atom nil)
         opts (r/atom {:randomizer :winkler
                       :sides 20
                       :times 1})]
     (fn []
-      [:div
-       [:div
-        [:select {:value (:randomizer @opts)
-                  :on-change #(swap! opts assoc :randomizer (-> % .-target .-value keyword))}
-         [:option {:value :winkler} "Winkler"]
-         [:option {:value :pseudo} "Psuedo"]
-         [:option {:value :uniform} "Uniform"]]
-        [:select {:value (:sides @opts)
-                  :on-change #(swap! opts assoc :sides (-> % .-target .-value int))}
-         [:option {:value 20} "d20"]
-         [:option {:value 12} "d12"]
-         [:option {:value 10} "d10"]
-         [:option {:value 8} "d8"]
-         [:option {:value 6} "d6"]
-         [:option {:value 4} "d4"]]
-        [:select {:value (:times @opts)
-                  :on-change #(swap! opts assoc :times (-> % .-target .-value int))}
-         (for [i (range 1 9)]
-           ^{:key (str "times-" i)}
-           [:option {:value i} i])]]
-       [:div
-        [:input {:type "button" :value "Click me!"
-                 :on-click #(reset! roll (dice/roll (init-randomizer @opts)))}]]
-       [roll-display @roll]])))
+      [:section.section
+       [:div.container
+        [:div.field.is-horizontal
+         [:div.field-label.is-normal
+          [:label.label "Randomizer"]]
+         [:div.field-body
+          [:div.field
+           [:div.control
+            [:div.select
+             [:select {:value (:randomizer @opts)
+                       :on-change #(swap! opts assoc :randomizer (-> % .-target .-value keyword))}
+              [:option {:value :winkler} "Winkler"]
+              [:option {:value :pseudo} "Psuedo"]
+              [:option {:value :uniform} "Uniform"]]]]]]]
+        [:div.field.is-horizontal
+         [:div.field-label.is-normal
+          [:label.label "Die type"]]
+         [:div.field-body
+          [:div.field
+           [:div.control
+            [:div.select
+             [:select {:value (:sides @opts)
+                       :on-change #(swap! opts assoc :sides (-> % .-target .-value int))}
+              [:option {:value 20} "d20"]
+              [:option {:value 12} "d12"]
+              [:option {:value 10} "d10"]
+              [:option {:value 8} "d8"]
+              [:option {:value 6} "d6"]
+              [:option {:value 4} "d4"]]]]]]]
+        [:div.field.is-horizontal
+         [:div.field-label.is-normal
+          [:label.label "Rolls"]]
+         [:div.field-body
+          [:div.field
+           [:div.control
+            [:div.select
+             [:select {:value (:times @opts)
+                       :on-change #(swap! opts assoc :times (-> % .-target .-value int))}
+              (for [i (range 1 9)]
+                ^{:key (str "times-" i)}
+                [:option {:value i} i])]]]]]]
+        [:div.field.is-horizontal
+         [:div.field-label]
+         [:div.field-body
+          [:div.field
+           [:div.control
+            [:button.button.is-link
+             {:on-click #(reset! roll (dice/roll (init-randomizer @opts)))}
+             "Roll"]]]]]
+        [roll-display @roll]]])))
 
 (defn app []
-  [roller])
+  [:<>
+   [header]
+   [roller]])
 
 (def a (atom {:randomizer :uniform}))
 (comment)
